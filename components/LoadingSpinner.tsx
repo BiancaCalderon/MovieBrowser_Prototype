56f24619eea86bd326f4ec888f0278a3736d47aa
Bianca <cal22272@uvg.edu.gt>
2025-04-03 10:54:27 -0600

    feat: animated loading component

---
+++ components/LoadingSpinner.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Animated, Easing } from 'react-native';
+
+export default function LoadingSpinner() {
+  const spinValue = new Animated.Value(0);
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(spinValue, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    ).start();
+  }, []);
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          styles.spinner,
+          {
+            transform: [{ rotate: spin }],
+          },
+        ]}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  spinner: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    borderWidth: 4,
+    borderColor: '#fff',
+    borderTopColor: 'rgba(255,255,255,0.2)',
+  },
+}); 
